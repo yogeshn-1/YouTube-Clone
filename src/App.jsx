fef89bd7173758038c3c785477fcb20fee9a844d
yogeshn-1 <nirmalkary@gmail.com>
2024-01-05 11:38:44 +0530

    Main page layout

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+const Container=styled.div`
+display:flex;
+`;
+const Main=styled.div`
+`;
+const Wrapper= styled.div``;
+
+function App() {
+  return (
+    <Container>
+      <Main>
+        <Wrapper>
+         video Cards 
+        </Wrapper>
+      </Main>
+    </Container>
+  )
+}
+
+export default App
