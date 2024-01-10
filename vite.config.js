7c5608883f54d8c05363a4b42b387953b04e6b13
yogeshn-1 <nirmalkary@gmail.com>
2024-01-10 12:37:03 +0530

    Adding dependencies for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/free-brands-svg-icons" ,'@fortawesome/free-brands-svg-icons',
+      '@fortawesome/react-fontawesome',
+      '@mui/icons-material/AccountCircle',
+      '@mui/icons-material/Explore',
+      '@mui/icons-material/Feed',
+      '@mui/icons-material/HelpCenter',
+      '@mui/icons-material/History',
+      '@mui/icons-material/Home',
+      '@mui/icons-material/LibraryAdd',
+      '@mui/icons-material/LibraryMusic',
+      '@mui/icons-material/LiveTv',
+      '@mui/icons-material/MovieCreation',
+      '@mui/icons-material/Report',
+      '@mui/icons-material/SettingsBrightness',
+      '@mui/icons-material/Settings',
+      '@mui/icons-material/SportsBasketball',
+      '@mui/icons-material/SportsEsports',
+      '@mui/icons-material/Subscriptions'],
+    },
+  },
 })
